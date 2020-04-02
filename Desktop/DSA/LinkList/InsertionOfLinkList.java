ddba94ead0ab45160d2f1fda798c97c8efb5f33e
AShu <ag9991323@gmail.com>
2020-04-02 16:40:50 +0530

    Adding solution of problems related to linklist in java

---
+++ Desktop/DSA/LinkList/InsertionOfLinkList.java
@@ -0,0 +1,77 @@
+package LinkList;
+import java.util.*;
+
+class Insertion{
+    Node head;
+
+    void insertAtBegining(int x){
+        Node curr = new Node(x);
+        curr.Next=head;
+        head=curr;
+        return;
+    }
+    void printLinkList(){
+        Node curr=head;
+        while(curr!=null){
+            System.out.print(curr.data+" ->");
+            curr=curr.Next;
+        }
+        System.out.println();
+    }
+    void insertatEnd(int x){
+        Node temp= new Node(x);
+        if(head==null){
+            head=temp;
+            return;
+        }
+        Node curr= head;
+        while(curr.Next!=null){
+            curr=curr.Next;
+        }
+        curr.Next=temp;
+        return;
+    }
+    void insertAtMiddle(int x){
+        Node temp = new Node(x);
+        if(head==null){
+            head=temp;
+            return;
+        }
+        if(head.Next==null){
+            head.Next=temp;
+            return;
+        }
+        Node first=head;
+        Node second=head;
+        Node prev=head;
+        while(second!=null&&second.Next!=null){
+            second=second.Next.Next;
+            prev=first;
+            first=first.Next;
+        }
+        if(second==null){
+            temp.Next=prev.Next;
+            prev.Next=temp;
+            return;
+        }
+        temp.Next=first.Next;
+            first.Next=temp;
+            return;
+      
+    }
+
+}
+class Main{
+    public static void main(String[] args) {
+        Insertion i = new Insertion();
+        i.insertatEnd(80);
+        i.insertatEnd(70);
+        i.insertatEnd(60);
+        i.insertatEnd(50);
+       
+       // i.insertatEnd(40);
+        i.printLinkList();
+       i.insertAtMiddle(5);
+       i.printLinkList();
+    }
+}
