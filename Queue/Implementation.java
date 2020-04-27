acce00339fc1a017776d683220b2dfac7c9fae14
AShu <ag9991323@gmail.com>
2020-04-27 17:09:21 +0530

    Adding implementation of queue using arrays in java

---
+++ Queue/Implementation.java
@@ -0,0 +1,57 @@
+package Queue;
+import java.util.*;
+class Queue{
+    int front=0;
+    int rear =0;
+    int size=0;
+    int cap =0;
+    int arr[];
+        Queue(int c){
+            cap=c;
+            arr = new int[cap];
+            
+        }
+        void enqueue(int c){
+            if(size==cap) return ;
+            arr[rear]=c;
+            rear=(rear+1)%cap;
+            size++;
+        }
+        void dequeue(){
+            if(size==0) return ;
+            arr[front]=0;
+            front=(front+1)%cap;
+            size--;
+        }
+}
+
+class  Main{
+      public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
+        
+          Queue q = new Queue(4);
+          q.enqueue(25);
+          System.out.println(Arrays.toString(q.arr));
+          q.enqueue(5);
+          System.out.println(Arrays.toString(q.arr));
+          q.dequeue();
+          q.enqueue(17);
+          q.enqueue(16);
+          q.enqueue(4);
+          q.enqueue(5);
+          System.out.println(Arrays.toString(q.arr));
+          q.enqueue(6);
+          System.out.println(Arrays.toString(q.arr));
+          q.dequeue();
+          System.out.println(Arrays.toString(q.arr));
+          System.out.println(q.arr[q.front]);
+          q.dequeue();
+          q.enqueue(20);
+          q.enqueue(20);
+          q.dequeue();
+          System.out.println(Arrays.toString(q.arr));
+        
+          
+          
+   }
+}
